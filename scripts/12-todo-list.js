a6fecea439255848da182634defa05b49f588244
Hud <hudalayubi@gmail.com>
2025-07-26 09:06:11 +0800

    Completed JS exercise and moving on to creating the Amazon project now

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -17,29 +17,37 @@ const todoList = [{
 
 function renderTodoList () {
   let todoListHTML = ''
+  // when passing Fn to another Fn use Arrow Fn
+  todoList.forEach((todoObject, index) => {
+  const {name, dueDate} = todoObject;
+      const html = `
+      <div>${name}</div>
+      <div>${dueDate} </div> 
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>
+      `;
+      todoListHTML += html;
+    
+    console.log(todoListHTML)
 
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    // const name = todoObject.name;
-    // const dueDate = todoObject.dueDate;
-    const {name, dueDate} = todoObject;
-    const html = `
-    <div>${name}</div>
-    <div>${dueDate} </div> 
-    <button onclick="
-      todoList.splice(${i}, 1);
-      renderTodoList();
-    " class="delete-todo-button">Delete</button>
-    `
-    todoListHTML += html 
-  }
-  console.log(todoListHTML)
+    document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML
 
-  document.querySelector('.js-todo-list')
-  .innerHTML = todoListHTML
-}
+    document.querySelectorAll('.js-delete-todo-button')
+      .forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', () => {
+          todoList.splice(index, 1);
+          renderTodoList();
+        })
+      })
+})}
+  
+   
   
 renderTodoList();
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo()
+})
 
 function addTodo() {
   const inputEl = document.querySelector(".js-name-input")
